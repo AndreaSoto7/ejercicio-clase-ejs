f7ae23eba3d065bfb1034743b8680d897c9540ee
AndreaSoto7 <739501@nur.edu.bo>
2026-04-15 23:25:24 -0400

    feat: add authentication routes

---
+++ routes/auth.routes.js
@@ -0,0 +1,16 @@
+const { checkUser } = require("../middlewares/check-user.js");
+
+module.exports = app => {
+    let router = require('express').Router();
+
+    const controller = require('../controllers/auth.controller');
+
+    router.get('/login', controller.loginGet);
+    router.post('/login', controller.loginPost);
+    router.get('/register', controller.registerGet);
+    router.post('/register', controller.registerPost);
+    router.get('/logout', checkUser, controller.logoutPost);
+
+    app.use('/inscripciones', router);
+
+}
