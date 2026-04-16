4a297d9bc3778b8929a72cb779359d413ff87880
AndreaSoto7 <739501@nur.edu.bo>
2026-04-16 01:04:09 -0400

    refactor: move route registration to routes index and remove controllers index

--- controllers/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app, db) => {
-    require('./home.controller')(app, db);
-    require('./persona.controller')(app, db);
-    require('./mascota.controller')(app, db);
-    require('./auth.controller')(app, db);
-}
